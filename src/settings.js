e88b1a119bd1de04218dfcc31b361a26f418cea2
wen <358079780@qq.com>
2022-03-17 22:04:37 +0800

    添加 svg

---
+++ src/settings.js
@@ -0,0 +1,36 @@
+module.exports = {
+  title: "极乐云",
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the settings right-panel
+   */
+  showSettings: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false,
+
+  /**
+   * @type {string | array} 'production' | ['production', 'development']
+   * @description Need show err logs component.
+   * The default is only used in the production env
+   * If you want to also use it in dev, you can pass ['production', 'development']
+   */
+  errorLog: "production",
+
+  /**
+   * @type {string}
+   * @description app client credential
+   */
+  client_id: "RW5JZTFaWUpsaXR2d1kxMw",
+  client_secret: "WmdmRzVXMENQRFZLLWg3d3RuSWRjZEk5SHNvTWg3Q08",
+};
